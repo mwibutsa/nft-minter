fc9a417d1045b0c8116c178013da4dbf0a2506e7
mwibutsa <mflohost@gmail.com>
2023-09-15 10:47:21 +0200

    Update

--- backend/scripts/MultiToken_deploy.js
+++ backend/scripts/MultiToken_deploy.js
@@ -3,7 +3,6 @@ const hre = require("hardhat");
 async function main() {
   const Contract = await hre.ethers.getContractFactory("MultiToken");
   const contract = await Contract.deploy();
-
   await contract.deployed();
 
   console.log("MultiToken deployed to:", contract.address);
