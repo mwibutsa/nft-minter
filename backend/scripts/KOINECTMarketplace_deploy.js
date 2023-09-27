b502cd1a743c7034bcaa78b7eeb86032ab4ac199
mwibutsa <mflohost@gmail.com>
2023-09-27 14:24:01 +0200

    After resolving conflicts.

---
+++ backend/scripts/KOINECTMarketplace_deploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Contract = await hre.ethers.getContractFactory("KOINECTMarketplace");
+  const contract = await Contract.deploy();
+  await contract.deployed();
+
+  console.log("KOINECTMarketplace deployed to:", contract.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
